f1c28a1df0f181140df84e4eece7d9016abc4f91
lenovo 3 <lenovo 3>
2020-09-24 19:12:36 +0200

    method Add() return zero for un empty string

--- test/string-calculator.spec.js
+++ test/string-calculator.spec.js
@@ -1,5 +1,19 @@
+const Add = ( string ) => {
+    if ( string === "") {
+        return  0
+    }
+}
+
+
+
 describe('white test', () => {
     it('should return true', () => {
         expect(true == true ).toStrictEqual(true);
+    });
+});
+
+describe('String Calculator', () => {
+    test('method Add() return zero for un empty string ', () => {
+        expect(Add("")).toStrictEqual(0)
     });
 });
