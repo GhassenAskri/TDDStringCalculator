8e157c34d8152c56129ae4af32e89a8e6cae4a5d
lenovo 3 <lenovo 3>
2020-09-24 19:15:44 +0200

    method Add() return one for a string = 1'

--- test/string-calculator.spec.js
+++ test/string-calculator.spec.js
@@ -2,6 +2,9 @@ const Add = ( string ) => {
     if ( string === "") {
         return  0
     }
+    if ( string === "1") {
+        return 1
+    }
 }
 
 
@@ -16,4 +19,7 @@ describe('String Calculator', () => {
     test('method Add() return zero for un empty string ', () => {
         expect(Add("")).toStrictEqual(0)
     });
+    test('method Add() return one for a string = "1',()=>{
+        expect(Add("1")).toStrictEqual(1)
+    })
 });
