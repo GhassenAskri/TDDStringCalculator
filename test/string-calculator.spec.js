509169e60585810acca338e5f1c9c1d584b24df9
lenovo 3 <lenovo 3>
2020-09-25 08:40:21 +0200

    method Add() return three for a string = 1,2

--- test/string-calculator.spec.js
+++ test/string-calculator.spec.js
@@ -1,18 +1,17 @@
-const Add = ( string ) => {
-    if ( string === "") {
-        return  0
+const Add = (string) => {
+    if (string === "") {
+        return 0
     }
-    const numbers  = string.split(",");
+    const numbers = string.split(",");
 
-    return parseInt(numbers.reduce( (number,val) => number + val  ,""))
+    return numbers.map(element => parseInt(element)).reduce((acc, val) => acc + val, 0)
 
 }
 
 
-
 describe('white test', () => {
     it('should return true', () => {
-        expect(true == true ).toStrictEqual(true);
+        expect(true == true).toStrictEqual(true);
     });
 });
 
@@ -20,10 +19,10 @@ describe('String Calculator', () => {
     test('method Add() return zero for un empty string ', () => {
         expect(Add("")).toStrictEqual(0)
     });
-    test('method Add() return one for a string = "1',()=>{
+    test('method Add() return one for a string = "1', () => {
         expect(Add("1")).toStrictEqual(1)
     })
-    test('method Add() return one for a string = "1',()=>{
-        expect(Add("1,2")).toStrictEqual(12)
+    test('method Add() return one for a string = "1,2', () => {
+        expect(Add("1,2")).toStrictEqual(3)
     })
 });
