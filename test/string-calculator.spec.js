e2c23adf7706d39d0771f7942463ca000b3fc55a
lenovo 3 <lenovo 3>
2020-10-04 19:10:32 +0200

    Refactoring : extract method

--- test/string-calculator.spec.js
+++ test/string-calculator.spec.js
@@ -1,10 +1,20 @@
+function parseStringToNumbers(string) {
+    const regex = /\n/gi;
+    stringToBeSplited = string.replace(regex, ",")
+    const numbers = stringToBeSplited.split(",");
+    return numbers.map(element => {
+        return parseInt(element)
+    })
+}
+
 const Add = (string) => {
+
     if (string === "") {
         return 0
     }
-    const numbers = string.split(",");
+    const numbers = parseStringToNumbers(string);
 
-    return numbers.map(element => parseInt(element)).reduce((acc, val) => acc + val, 0)
+    return  numbers.reduce((acc, val) => acc + val, 0);
 
 }
 
@@ -15,14 +25,27 @@ describe('white test', () => {
     });
 });
 
-describe('String Calculator', () => {
+describe('handle comas between numbers', () => {
     test('method Add() return zero for un empty string ', () => {
         expect(Add("")).toStrictEqual(0)
     });
     test('method Add() return one for a string = "1', () => {
         expect(Add("1")).toStrictEqual(1)
     })
-    test('method Add() return one for a string = "1,2', () => {
+    test('method Add() return three for a string = "1,2', () => {
         expect(Add("1,2")).toStrictEqual(3)
     })
+
+});
+describe('handle new lines between numbers', () => {
+    test('method Add() six zero for "1\n2,3"', () => {
+        expect(Add("1\n2,3")).toStrictEqual(6)
+    });
+    test('method Add() six zero for "1\n2,3\n2"', () => {
+        expect(Add("1\n2,3\n2")).toStrictEqual(8)
+    });
+    test('method Add() six zero for "1\n2,3\n2"', () => {
+        expect(Add("1\n2\n3\n2")).toStrictEqual(8)
+    });
+
 });
